8d50e8452975b14c288378b9c1ed26ea159b1a3e
june lee <melodysdreamj@gmail.com>
2024-07-18 17:12:18 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/dot_navigation_bar_basic_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForDotNavigationBarBasicLego(BuildContext context) async {}
